d51a094628024e7720670e848a0ef30060c10de6
cjjujubee <connie.jew@gmail.com>
2016-04-14 11:37:22 -0700

    Adds questions and submit button

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// Question List
+var question1 = {
+	question: 'question1',
+	options: ['option1', 'option2', 'option3', 'option4'], 
+	correctAnswer: 0
+}
+
+
+
+var question2 = {
+	question: 'question2',
+	options: ['option1', 'option2', 'option3', 'option4'], 
+	correctAnswer: 0
+}
+
+
+
+
+var question3 = {
+	question: 'question3',
+	options: ['option1', 'option2', 'option3', 'option4'], 
+	correctAnswer: 0
+}
+
+
+
+
+var question4 = {
+	question: 'question4',
+	options: ['option1', 'option2', 'option3', 'option4'], 
+	correctAnswer: 0
+}
+
+
+
+
+var question5 = {
+	question: 'question5',
+	options: ['option1', 'option2', 'option3', 'option4'], 
+	correctAnswer: 0
+}
+
+var listOfQuestions = [question1, question2, question3, question4, question5]
+
+// Submit Button //
+
+$('#submitButton').click(function() {
+	
+})
