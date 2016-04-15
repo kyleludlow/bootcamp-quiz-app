8a1e040b8ebb0e096dfa9d8042f00ca207ba9945
Kyle Ludlow <kerrymurphy@Kerrys-iMac.local>
2016-04-14 18:47:54 -0700

    adds new game functionality, refactored code

--- js/app.js
+++ js/app.js
@@ -1,110 +1,111 @@
-// Question List
-var question1 = {
-	question: 'question1',
-	options: ['option1', 'option2', 'option3', 'option4'], 
-	correctAnswer: 0
-}
+//Define Objects
+var listOfQuestions = [{
+    question: 'question1',
+    options: ['Correct Answer', 'option2', 'option3', 'option4'],
+    correctAnswer: 0
+}, {
+    question: 'question2',
+    options: ['question 2 option1', 'Correct Answer', 'option3', 'option4'],
+    correctAnswer: 1
+}, {
+    question: 'question3',
+    options: ['question 3 option1', 'option2', 'Correct Answer', 'option4'],
+    correctAnswer: 2
+}, {
+    question: 'question4',
+    options: ['question 4 option1', 'option2', 'option3', 'Correct Answer'],
+    correctAnswer: 3
+}, {
+    question: 'question5',
+    options: ['Correct Answer', 'option2', 'option3', 'option4'],
+    correctAnswer: 0
+}];
 
-
-
-var question2 = {
-	question: 'question2',
-	options: ['question 2 option1', 'option2', 'option3', 'option4'],
-	correctAnswer: 0
-}
-
-
-
-
-var question3 = {
-	question: 'question3',
-	options: ['question 3 option1', 'option2', 'option3', 'option4'],
-	correctAnswer: 0
-}
-
-
-
-
-var question4 = {
-	question: 'question4',
-	options: ['option1', 'option2', 'option3', 'option4'], 
-	correctAnswer: 0
-}
-
-
-
-
-var question5 = {
-	question: 'question5',
-	options: ['option1', 'option2', 'option3', 'option4'], 
-	correctAnswer: 0
-}
-
-var listOfQuestions = [question1, question2, question3, question4, question5];
+//Define global variables
 
 var currentQuestion = 0;
 
 var userScore = 0;
 
-$('p > input').change(function() {
 
-	$('#submitButton').prop('disabled', false).removeClass('disabled');
+
+
+//Define global functions
+var displayNewQuestion = function () {
+    $('#question').text(listOfQuestions[currentQuestion].question);
+
+};
+
+var displayNewAnswerChoices = function () {
+    for (var i = 0; i < 4; i++) {
+
+        $('#choice' + (i + 1)).text(listOfQuestions[currentQuestion].options[i]);
+
+    }
+};
+
+var resetRadioButtons = function () {
+    $('input[name="userGuess"]').prop('checked', false);
+};
+
+var changeUserScoreTally = function () {
+    $('#userScoreTally').text(userScore);
+};
+
+var newGame = function () {
+    currentQuestion = 0;
+    userScore = 0;
+    resetRadioButtons();
+    changeUserScoreTally();
+    $('#submitButton').show();
+    $('#newGameButton').hide();
+    displayNewQuestion();
+    displayNewAnswerChoices();
+};
+
+newGame();
+
+$('#submitButton').click(function (event) {
+    event.preventDefault();
+
+    //captures user selection
+    var currentUserAnswer = $("input[name=userGuess]:checked").val();
+
+    // gets current correct answer
+    var currentCorrectAnswer = listOfQuestions[currentQuestion].correctAnswer;
+
+    if (currentUserAnswer == currentCorrectAnswer) {
+        userScore++;
+    }
+
+    changeUserScoreTally();
+
+    currentQuestion++;
+
+    if (currentQuestion > 4) {
+
+        $('#submitButton').hide();
+        $('#newGameButton').show();
+    } else {
+
+        //replace question with new
+        displayNewQuestion();
+
+        //replace list of answer choices
+        displayNewAnswerChoices();
+
+    }
+
+    resetRadioButtons();
 });
 
-// Submit Button //
-
-$('#submitButton').click(function(event) {
-	event.preventDefault();
-
-	//remove old question and answers
-
-
-	//go to next question
-
-
-	//replace question
-
-	$('#question').text(listOfQuestions[currentQuestion].question);
-
-
-	
-	//captures user selection
-	var currentUserAnswer = $("input[name=userGuess]:checked").val();
-
-	//replace list of answers
-
-	for(var i = 0; i < 4; i++ ) {
-
-		$('#choice' + (i + 1) ).text(listOfQuestions[currentQuestion].options[i]);
-
-	}
-
-	//resets radio button
-
-	$('input[name="userGuess"]').prop('checked', false);
-
-
-	// gets correct answers
-
-	var currentCorrectAnswer = listOfQuestions[currentQuestion].correctAnswer;
-
-	if (currentUserAnswer === currentCorrectAnswer) {
-		userScore++
-		console.log(userScore++);
-	}
-
-	$('.userScoreTally').text(userScore);
-
-
-	//increment currentQuestion
-
-	currentQuestion++;
-
-
-
-
-
-
-
-
+$('#newGameButton').click(function (event) {
+    event.preventDefault();
+    newGame();
 });
+
+$('p > input').change(function () {
+
+    $('#submitButton').prop('disabled', false).removeClass('disabled');
+});
+
